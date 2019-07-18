82a90d61f0f235981b4eb663d7fec306c92a328f
Hedersonleeh <hedersoncarrasquero@gmail.com>
2019-07-18 01:13:34 -0500

    added simple 2d move

---
+++ Assets/Code/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using NaughtyAttributes;
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField, Range(0f, 100f), BoxGroup("Player")] private float health = 100;
+    [SerializeField, Range(0, 4f), BoxGroup("Player")] private float moveSpeed;
+
+    [SerializeField, BoxGroup("Player")] private Rigidbody2D rb;
+    [SerializeField, BoxGroup("Player")] private float jumpFonce;
+
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxis("Horizontal")*(rb.velocity.x+moveSpeed*100)*Time.fixedDeltaTime, rb.velocity.y);
+    }
+    public Rigidbody2D Rb
+    {
+        get { return rb; }
+    }
+
+}
---
+++ Assets/PlayerMonitor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+#if(UNITY_EDITOR)
+public class PlayerMonitor : MonoBehaviour {
+
+	[SerializeField]Rigidbody2D playerRb;
+	[SerializeField]Text txt;
+	
+	void Update () 
+	{
+		
+		txt.text = string.Concat(Mathf.Abs(Mathf.Round(playerRb.velocity.x)),"m/s");	
+	}
+}
+#endif
